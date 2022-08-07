ad8eeb5ab52b4ac904efaf0288d1f811318aabfc
Kevin Hughes <kevin.hughes@aiven.io>
2022-08-07 15:13:56 -0400

    wip

--- font-patcher.py
+++ font-patcher.py
@@ -7,19 +7,52 @@ import pathlib
 
 for f in pathlib.Path("./src_fonts").glob("*"):
     font = fontforge.open(str(f))
+    font_height = font.os2_windescent + font.os2_winascent
+
+    # centered reference char?
+    side = 0xe0b0  # 
 
     separators = [
-        0xe0ba,
-        0xe0bc,
-        0xe0b8,
-        0xe0be,
+        0xe0ba,  # 
+
+        0xe0bc,  # 
+
+        0xe0b8,  # 
+
+        0xe0be,  # 
     ]
 
     scale = psMat.scale(0.5, 1)
-    translate = psMat.translate(0, 10)
+    translate = psMat.translate(0, -48)
 
     for sep in separators:
-        font[sep].foreground = font[sep].foreground.transform(scale)
-        font[sep].foreground = font[sep].foreground.transform(translate)
+        font[sep].transform(scale)
+        font[sep].transform(translate)
+
+    # lsep 
+    # needs to be moved down
+    # sep = 0xe0b8
+    # translate = psMat.translate(-1, -12)
+    # font[sep].foreground = font[sep].foreground.transform(translate)
+
+    # lsep2 
+    # needs to be moved up
+    # sep = 0xe0be
+    # translate = psMat.translate(1, 12)
+    # font[sep].foreground = font[sep].foreground.transform(translate)
+
+    # rsep 
+    # needs to be moved up
+    # sep = 0xe0ba
+    # translate = psMat.translate(1, 12)
+    # font[sep].foreground = font[sep].foreground.transform(translate)
+
+    # for sep in separators:
+    #     print(font[sep].foreground.boundingBox())
+    #
+    # breakpoint()
 
     font.generate(f"./fonts/{f.name}", flags=(str('opentype')))
+
+# echo test:
+# echo -e "\ue0be\e[1;42m BG \e[0m\ue0b8"
